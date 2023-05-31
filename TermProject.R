426ab679eb19429211b1e18f47be354cda060869
Russell Chien <RussellChien6@gmail.com>
2023-05-31 00:01:28 -0700

    classify columns into families

--- TermProject.R
+++ TermProject.R
@@ -2,4 +2,16 @@
 load('law.school.admissions.rda',verbose=T) 
 
 print(head(law.school.admissions))
-hist(law.school.admissions$lsat) 
+hist(law.school.admissions$fam_inc) 
+
+# normal family
+# use lsat or fam_inc
+
+# exponential family
+# idk
+
+# gamma family
+# use lsat or fam_inc
+
+# beta family
+# use ugpa
