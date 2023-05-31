0e2d0658ce7c9b741d3d795d316082a14c321b55
Russell Chien <RussellChien6@gmail.com>
2023-05-31 11:14:14 -0700

    add save plot function

--- TermProject.R
+++ TermProject.R
@@ -1,8 +1,31 @@
 # load in 'law.school.admissions.rda'
 load('law.school.admissions.rda',verbose=T) 
 
+# data analysis 
 print(head(law.school.admissions))
 hist(law.school.admissions$fam_inc) 
+
+# image type is implied by the file name suffix, e.g. '.jpg'; other choices are '.pdf' and '.png'
+pr2file <- function (filename)
+{
+    origdev <- dev.cur()
+    parts <- strsplit(filename,".",fixed=TRUE)
+    nparts <- length(parts[[1]])
+    suff <- parts[[1]][nparts]
+    if (suff == "pdf") {
+        pdf(filename)
+    }
+    else if (suff == "png") {
+        png(filename,bg='white')
+    }
+    else jpeg(filename)
+    devnum <- dev.cur()
+    dev.set(origdev)
+    dev.copy(which = devnum)
+    dev.set(devnum)
+    dev.off()
+    dev.set(origdev)
+}
 
 # normal family
 # use lsat or fam_inc
